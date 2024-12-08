303545d7cdc6b7efacb5611d4ece366783743c7b
jf-fullstack <joaofelipe.jfos@outlook.com>
2024-12-08 17:07:42 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 function Home() {
     return<h1>
-        ALERTA DE SEGURANÇA
-        Sua conta Iti acaba de ser clonada
-        CPF: ***.596.767-**
-        Agência: 0500
-        Número da conta: 026293144-2
-        Instituição: BANCO ITAUCARD S.A.
-        Chave Pix: 190.596.767-50
-        Nome: NICOLAS BRILHANTE
-        ATENÇÃO: você possivelmente foi exposto a um ataque cibernético. Aumente a segurança da sua conta para evitar possíveis danos irreparáveis.
+        jf é pika
     </h1>
 }
 
